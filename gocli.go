fcb6f55a28f3c6f7272e4cfe5aadc82ca2e84c1e
leep-frog <gleeper49@gmail.com>
2022-05-30 08:50:49 -0400

    small changes

--- gocli.go
+++ gocli.go
@@ -2,7 +2,7 @@ package gocli
 
 import (
 	"regexp"
-	"strings"
+	"strconv"
 
 	"github.com/leep-frog/command"
 	"github.com/leep-frog/command/sourcerer"
@@ -21,6 +21,7 @@ func (gc *gocli) Name() string    { return "gt" }
 var (
 	pathArgs      = command.ListArg[string]("PATH", "Path(s) to go packages to test", 0, command.UnboundedList, &command.FileCompletor[[]string]{Distinct: true, IgnoreFiles: true})
 	coverageRegex = regexp.MustCompile(`coverage: +([0-9]+\.[0-9]+)% of statements$`)
+	noTestRegex   = regexp.MustCompile(`^\?.*\[no test files\]$`)
 )
 
 func (gc *gocli) Node() *command.Node {
@@ -30,19 +31,28 @@ func (gc *gocli) Node() *command.Node {
 			command.ExecuteErrNode(func(o command.Output, d *command.Data) error {
 				res, err := command.NewBashCommand[[]string]("", []string{"go test . -coverprofile=$(mktemp)"}, command.ForwardStdout[[]string]()).Run(o)
 				if err != nil {
-					return o.Annotate(err, "failed to run go test command")
+					// Failed to build or test failed so just return
+					return err
 				}
 
-				covIdx := len(res) - 1
-				for ; covIdx > 0 && (strings.TrimSpace(res[covIdx]) == "" || res[covIdx][:4] == "FAIL"); covIdx-- {
+				var m []string
+				for covIdx := len(res) - 1; covIdx >= 0 && len(m) == 0; covIdx-- {
+					m = coverageRegex.FindStringSubmatch(res[covIdx])
+					if noTestRegex.MatchString(res[covIdx]) {
+						o.Stdoutf("No test files")
+						return nil
+					}
 				}
-				covLine := res[covIdx]
-				m := coverageRegex.FindStringSubmatch(covLine)
 				if len(m) == 0 {
-					return o.Stderrf("faield to parse coverage info from line %q", covLine)
+					return o.Stderrf("failed to parse coverage info")
 				}
 
-				o.Stdoutf("Coverage is %q", m[1])
+				f, err := strconv.ParseFloat(m[1], 64)
+				if err != nil {
+					return o.Annotate(err, "failed to parse coverage value")
+				}
+
+				o.Stdoutf(`Coverage is %2.2f\%`, f)
 				return nil
 			}),
 		),
