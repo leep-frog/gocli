e6b31e762d5e9f652915bd59ffa45f6ea72d5678
leep-frog <gleeper49@gmail.com>
2022-09-13 14:04:35 -0400

    update command package

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.18
 
 require (
 	github.com/google/go-cmp v0.5.5 // indirect
-	github.com/leep-frog/command v0.0.0-20220908170245-84c655f5a5ed // indirect
+	github.com/leep-frog/command v0.0.0-20220913175955-ecdd7a1059e5 // indirect
 	golang.org/x/exp v0.0.0-20220321124402-2d6d886f8a82 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 )
--- gocli.go
+++ gocli.go
@@ -27,7 +27,7 @@ var (
 	// Args and flags
 	pathArgs        = command.ListArg[string]("PATH", "Path(s) to go packages to test", 0, command.UnboundedList, &command.FileCompleter[[]string]{Distinct: true, IgnoreFiles: true}, command.Default([]string{"."}))
 	verboseFlag     = command.BoolFlag("verbose", 'v', "Whether or not to test with verbose output")
-	minCoverageFlag = command.NewFlag[float64]("MIN_COVERAGE", 'm', "If set, enforces that minimum coverage is met", command.Positive[float64](), command.LTE[float64](100), command.Default[float64](0))
+	minCoverageFlag = command.Flag[float64]("MIN_COVERAGE", 'm', "If set, enforces that minimum coverage is met", command.Positive[float64](), command.LTE[float64](100), command.Default[float64](0))
 )
 
 func percentFormat(f float64) string {
@@ -35,53 +35,51 @@ func percentFormat(f float64) string {
 }
 
 func (gc *goCLI) Node() *command.Node {
-	return command.BranchNode(map[string]*command.Node{},
-		command.SerialNodes(
-			command.NewFlagNode(
-				minCoverageFlag,
-				verboseFlag,
-			),
-			pathArgs,
-			command.ExecuteErrNode(func(o command.Output, d *command.Data) error {
-				// Error if verbose and coverage check
-				mc := minCoverageFlag.Get(d)
-				if verboseFlag.Get(d) && mc != 0 {
-					return o.Stderrln("Can't run verbose output with coverage checks")
-				}
-
-				res, err := command.NewBashCommand("", []string{fmt.Sprintf("go test %s -coverprofile=$(mktemp)", strings.Join(pathArgs.Get(d), " "))}, command.ForwardStdout[[]string]()).Run(o, d)
-				if err != nil {
-					// Failed to build or test failed so just return
-					return o.Err(err)
-				}
-
-				// Error to return
-				var retErr error
-				for _, coverage := range res {
-					if coverage == "" {
-						continue
-					}
-
-					if noTestRegex.MatchString(coverage) {
-						continue
-					}
-
-					m := coverageRegex.FindStringSubmatch(coverage)
-					if len(m) == 0 {
-						return o.Stderrf("failed to parse coverage from line %q\n", coverage)
-					}
-
-					f, err := strconv.ParseFloat(m[2], 64)
-					if err != nil {
-						return o.Annotate(err, "failed to parse coverage value")
-					}
-
-					if f < mc {
-						retErr = o.Stderrf("Coverage of package %q (%s) must be at least %s\n", m[1], percentFormat(f), percentFormat(mc))
-					}
-				}
-				return retErr
-			}),
+	return command.SerialNodes(
+		command.FlagNode(
+			minCoverageFlag,
+			verboseFlag,
 		),
+		pathArgs,
+		&command.ExecutorProcessor{F: func(o command.Output, d *command.Data) error {
+			// Error if verbose and coverage check
+			mc := minCoverageFlag.Get(d)
+			if verboseFlag.Get(d) && mc != 0 {
+				return o.Stderrln("Can't run verbose output with coverage checks")
+			}
+
+			res, err := command.NewBashCommand("", []string{fmt.Sprintf("go test %s -coverprofile=$(mktemp)", strings.Join(pathArgs.Get(d), " "))}, command.ForwardStdout[[]string]()).Run(o, d)
+			if err != nil {
+				// Failed to build or test failed so just return
+				return o.Err(err)
+			}
+
+			// Error to return
+			var retErr error
+			for _, coverage := range res {
+				if coverage == "" {
+					continue
+				}
+
+				if noTestRegex.MatchString(coverage) {
+					continue
+				}
+
+				m := coverageRegex.FindStringSubmatch(coverage)
+				if len(m) == 0 {
+					return o.Stderrf("failed to parse coverage from line %q\n", coverage)
+				}
+
+				f, err := strconv.ParseFloat(m[2], 64)
+				if err != nil {
+					return o.Annotate(err, "failed to parse coverage value")
+				}
+
+				if f < mc {
+					retErr = o.Stderrf("Coverage of package %q (%s) must be at least %s\n", m[1], percentFormat(f), percentFormat(mc))
+				}
+			}
+			return retErr
+		}},
 	)
 }
